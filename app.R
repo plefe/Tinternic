18b45509abf46b487b81cb3a2ee48369ce13553c
plefe <parker.lefebvre@nmhs.org>
2023-07-20 13:10:23 -0500

    brainstorming

--- app.R
+++ app.R
@@ -7,3 +7,9 @@ library(highcharter)
 library(shiny)
 library(shinyWidgets)
 library(shinydashboard)
+
+#brainstormed ideas:
+# I want two tabs, male and female. Main filter being whether they survived or not
+# Male tab: 4 charts. class, age, cabin, embarked
+# Female tab: 4 charts. class, age, cabin, embarked
+# tab 3: ML tab covering what had the greatest impact on survival/class/fare/age
