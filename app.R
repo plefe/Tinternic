2d3fa13142640eb7e5f848bc4e38dbc53c89ce12
plefe <parker.lefebvre@nmhs.org>
2023-07-20 12:58:51 -0500

    read in data. added libraries

---
+++ app.R
@@ -0,0 +1,9 @@
+train <- read.csv("train.csv")
+test <- read.csv("test.csv")
+
+library(reactable)
+library(tidyverse)
+library(highcharter)
+library(shiny)
+library(shinyWidgets)
+library(shinydashboard)
