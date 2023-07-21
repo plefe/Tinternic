f64a670af4ee0d0e90a72262eef70a86fd520706
plefe <parker.lefebvre@nmhs.org>
2023-07-21 09:14:26 -0500

    app under contruction

--- app.R
+++ app.R
@@ -10,6 +10,7 @@ test <- read.csv("test.csv")
 
 full <- bind_rows(train, test)
 str(full)
+str(men)
 
 
 
@@ -19,11 +20,44 @@ str(full)
 # Female tab: 4 charts. class, age, cabin, embarked
 # tab 3: ML tab covering what had the greatest impact on survival/class/fare/age
 
+ui <- dashboardPage(
+  dashboardHeader(title = "Tinternic"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Men Analysis", tabName = "mens", icon = icon("person")),
+      menuItem("Women Analysis", tabName = "womens", icon = icon("person-dress"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "mens",
+              fluidRow(column(width = 6, highchartOutput('class_man')),
+                       column(width = 6, highchartOutput(''))),
+              fluidRow(column(width = 6, highchartOutput('')),
+                       column(width = 6, highchartOutput('')))),
+      tabItem(tabName = "womens",
+              fluidRow(column(width = 6),
+                       column(width = 6)),
+              fluidRow(column(width = 6),
+                       column(width = 6))))
+    )
+  )
+
+
+
 output$class_man <- renderHighchart({
   men <- full[full$Sex == "male", ] %>% 
+    men %>% 
+    mutate(Pclass = case_when(
+      Pclass == 1 ~ "First class",
+      Pclass == 2 ~ "Second class",
+      Pclass == 3 ~ "Third class")) %>% 
   count(Pclass) %>% 
-    arrange(n) %>% 
-    hchart(men, type = "treemap", hcaes(x = Pclass, value = n, color = n))
+  arrange(n) %>% 
+    hchart(type = "treemap", hcaes(x = Pclass, value = n, color = n)) %>% 
+    hc_title(text = "Class Distribution") %>%
+    hc_tooltip(pointFormat = "Class: {point.category}<br/>Pclass") %>% 
+    hc_add_theme(hc_theme_smpl())
 })
 hchart(heatmap)
 
